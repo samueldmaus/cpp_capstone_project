a7de02e2ca7df6045f43abc6984249b0a6688071
Sam Maus <samueldmaus@gmail.com>
2020-12-14 20:29:10 -0600

    getting time to draw on window

--- src/app_gui.cpp
+++ src/app_gui.cpp
@@ -1,17 +1,40 @@
+#include <iostream>
+#include <sstream>
+#include <ctime>
 #include "app_gui.h"
 
-IMPLEMENT_APP(MyApp)
-
-bool MyApp::OnInit()
-{
-    AppFrame *simple = new AppFrame(wxT("Simple"));
-    simple->Show(true);
-
+bool MyApp::OnInit() {
+    // create window with name and shows it
+    AppFrame *my_app = new AppFrame(wxT("My App"));
+    my_app->Show(true);
+    SetTopWindow(my_app);
     return true;
 }
 
+// create wxWidgets frame
 AppFrame::AppFrame(const wxString& title)
-       : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
+       : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(400, 400))
 {
+  Connect(wxEVT_PAINT, wxPaintEventHandler(AppFrame::OnPaint));
   Centre();
 }
+
+// draw the month, day, year, time (DrawText has to use stringstream)
+void AppFrame::OnPaint(wxPaintEvent& event) {
+  wxPaintDC dc(this);
+  while(true) {
+    auto dt = AppFrame::GetTime();
+    std::stringstream str;
+    str << dt;
+    dc.DrawText(str.str().c_str(), 20, 20);
+  }
+}
+
+char* AppFrame::GetTime() {
+  std::time_t now = time(0);
+  char* dt = ctime(&now);
+  return dt;
+}
+
+// implement wxWidgets application
+IMPLEMENT_APP(MyApp)
--- src/app_gui.h
+++ src/app_gui.h
@@ -2,19 +2,23 @@
 #define APP_GUI_H
 
 #include <wx/wx.h>
+#include <ctime>
 
-
+// wxWidgets Frame that controls all elements
 class AppFrame : public wxFrame
 {
 public:
     AppFrame(const wxString& title);
-
+    void OnPaint(wxPaintEvent& event);
+    char* GetTime();
 };
 
+// wxWidgets app that hides main()
 class MyApp : public wxApp
 {
   public:
     virtual bool OnInit();
 };
+
 
 #endif
